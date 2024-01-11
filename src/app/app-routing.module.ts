b6d2a818aae67a0a1101e14ea33d637e3c8c77f0
Your Ahmed <ah1942892@gmail.com>
2024-01-11 11:39:11 +0200

    Add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/core/home/home.component';
+import { CartComponent } from './components/core/cart/cart.component';
+import { CategoriesComponent } from './components/core/categories/categories.component';
+import { AboutUsComponent } from './components/layout/about-us/about-us.component';
+import { BrandsComponent } from './components/core/brands/brands.component';
+import { NotFoundComponent } from './components/layout/not-found/not-found.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+   redirectTo:'home',
+   pathMatch:'full'
+  },
+  {path:'home',
+   component:HomeComponent
+  }
+  ,{path:'cart',
+   component:CartComponent
+  }
+  ,{path:'categories',
+   component:CategoriesComponent
+  }
+  ,{path:'about-us',
+   component:AboutUsComponent
+  }
+  ,{path:'brands',
+   component:BrandsComponent
+  }
+  ,{path:'login',
+   component:LoginComponent
+  }
+  ,{path:'register',
+   component:RegisterComponent
+  }
+  ,{path:'**',
+   component:NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-main/app.component';
+import { NavBarComponent } from './components/layout/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { AboutUsComponent } from './components/layout/about-us/about-us.component';
+import { BrandsComponent } from './components/core/brands/brands.component';
+import { CategoriesComponent } from './components/core/categories/categories.component';
+import { CartComponent } from './components/core/cart/cart.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { NotFoundComponent } from './components/layout/not-found/not-found.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    AboutUsComponent,
+    BrandsComponent,
+    CategoriesComponent,
+    CartComponent,
+    HomeComponent,
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
