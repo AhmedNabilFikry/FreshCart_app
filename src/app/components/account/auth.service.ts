216cda4660aa4be232566790a06459edbf3f82da
Your Ahmed <ah1942892@gmail.com>
2024-01-12 13:15:58 +0200

    Update nav-bar Component

--- src/app/components/account/auth.service.ts
+++ src/app/components/account/auth.service.ts
@@ -1,33 +1,44 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private _HttpClient:HttpClient) { }
+  constructor(private _HttpClient:HttpClient, private _Router:Router) {
+    if (typeof(localStorage) !== 'undefined' && localStorage.getItem('userToken')) {
+      this.SaveUserData();
+    }
+  }
 
-  userdata = null;
+  userdata = new BehaviorSubject(null);
 
   SaveUserData(){
     let encodedToken = JSON.stringify(localStorage.getItem('userToken'));
     let decodedToken:any = jwtDecode(encodedToken);
-    this.userdata = decodedToken;
+    this.userdata.next(decodedToken);
     console.log(this.userdata);
   }
+
   SignUp(registerData:object):Observable<any>
-{
+  {
   return this._HttpClient.post('https://localhost:7144/api/Account/Register',registerData);
-}
+  }
 
-SignIn(loginData:object):Observable<any>
-{
+  SignIn(loginData:object):Observable<any>
+  {
   return this._HttpClient.post('https://localhost:7144/api/Account/Login',loginData);
-}
+  }
 
+  SignOut(){
+    localStorage.removeItem('userToken');
+    this.userdata.next(null);
+    this._Router.navigate(['/login'])
+  }
 
 
 
--- src/app/components/layout/nav-bar/nav-bar.component.ts
+++ src/app/components/layout/nav-bar/nav-bar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../../account/auth.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+  constructor(private _AuthService:AuthService){}
+  IsLogin:boolean = false;
+  userName:any = {};
+ngOnInit(): void {
+  this._AuthService.userdata.subscribe({
+    next: () =>{
+      if (this._AuthService.userdata.getValue() !== null) {
+        this.IsLogin = true;
+        this.userName=this._AuthService.userdata.getValue();
+      }else{
+        this.IsLogin = false;
+      }
+    }
+  })
+}
 
+SingOut() :void
+{
+  this._AuthService.SignOut();
+}
 }
